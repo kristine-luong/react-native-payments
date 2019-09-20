1d39f865667fcb6ebc607ee81d99c00aef703d7f
Vance Hu <vancehu@users.noreply.github.com>
2019-09-20 09:27:50 -0400

    Add type definition
    Added type definition for react-native-payments module. inherited from lib.dom.d.ts with tiny modification

---
+++ packages/react-native-payments/index.d.ts
@@ -0,0 +1,88 @@
+declare module '@brandingbrand/react-native-payments' {
+  export interface PaymentMethodData {
+    supportedMethods: string[];
+    data: {
+      merchantIdentifier: string;
+      supportedNetworks: string[];
+      countryCode: string;
+      currencyCode: string;
+    }
+  }
+
+
+  export interface PaymentCurrencyAmount {
+    currency: string;
+    currencySystem?: string;
+    value: string;
+  }
+
+  export interface PaymentItem {
+    amount: PaymentCurrencyAmount;
+    label: string;
+    pending?: boolean;
+  }
+
+  export interface PaymentShippingOption {
+    id: string;
+    label: string;
+    amount: PaymentCurrencyAmount;
+    detail: string;
+  }
+
+
+  export interface PaymentDetailsInit {
+    id: string;
+    shippingOptions?: PaymentShippingOption[];
+    displayItems: PaymentItem[];
+    total: PaymentItem;
+  }
+
+  export interface PaymentAddress {
+    readonly addressLine: string[];
+    readonly city: string;
+    readonly country: string;
+    readonly dependentLocality: string;
+    readonly languageCode: string;
+    readonly organization: string;
+    readonly phone: string;
+    readonly postalCode: string;
+    readonly recipient: string;
+    readonly region: string;
+    readonly sortingCode: string;
+    toJSON<T = any>(): T;
+  }
+
+  export type PaymentShippingType = "shipping" | "delivery" | "pickup";
+
+  export interface PaymentRequestEventMap {
+    "shippingaddresschange": PaymentRequestEvent;
+    "shippingoptionchange": PaymentRequestEvent;
+  }
+
+  export interface PaymentRequestEvent {
+    updateWith: (details: PaymentDetailsInit) => void;
+  }
+
+
+  export interface PaymentRequest {
+    readonly id: string;
+    onshippingaddresschange: ((this: PaymentRequest, ev: PaymentRequestEvent) => any) | null;
+    onshippingoptionchange: ((this: PaymentRequest, ev: PaymentRequestEvent) => any) | null;
+    readonly shippingAddress: PaymentAddress | null;
+    readonly shippingOption: string | null;
+    readonly shippingType: PaymentShippingType | null;
+    abort(): Promise<void>;
+    canMakePayment(): Promise<boolean>;
+    show(): Promise<PaymentResponse>;
+    addEventListener<K extends keyof PaymentRequestEventMap>(type: K, listener: (this: PaymentRequest, ev: PaymentRequestEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void;
+    addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void;
+    removeEventListener<K extends keyof PaymentRequestEventMap>(type: K, listener: (this: PaymentRequest, ev: PaymentRequestEventMap[K]) => any, options?: boolean | EventListenerOptions): void;
+    removeEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void;
+  }
+
+  export declare var PaymentRequest: {
+    prototype: PaymentRequest;
+    new(methodData: PaymentMethodData[], details: PaymentDetailsInit, options?: PaymentOptions): PaymentRequest;
+  };
+
+}
